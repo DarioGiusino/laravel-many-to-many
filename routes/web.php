97adbd60c2c5e8f0637d5c006165aa1437bec161
DarioGiusino <dario.giusino@gmail.com>
2023-03-13 14:35:28 +0100

    installed make:crud package + made technology entitity env

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::middleware('auth')->name('admin.')->prefix('/admin')->group(function () {
     Route::resource('projects', ProjectController::class);
     Route::resource('types', TypeController::class);
     Route::patch('types/{type}/patch', [TypeController::class, 'patch'])->name('types.patch');
+    Route::resource('technologies', TechnologyController::class);
 });
 
 
